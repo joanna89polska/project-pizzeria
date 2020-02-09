85cca3a608666bcc7abb7b29ea03c980bcaab1d0
Joanna Polska <joanna19polska@gmail.com>
2020-02-09 22:04:45 +0100

    new page booking added

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,81 @@
-import {settings, select, classNames, templates} from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    /* thisApp.activatePage(thisApp.pages[0].id); always the first page would be activated even if the website was refreshed */
+    const idFromHash = window.location.hash.replace('#/', '');
+    //console.log('idFromHash', idFromHash);
+
+    /* how to find a page with existing Id */
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    //console.log('pageMatchingHash', pageMatchingHash);
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash - when added with '/' it won't move to the part of the page with this id but instead it will show all page*/
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      //if(page.id == pageId){
+      //  page.classList.add(classNames.pages.active);
+      //} else {
+      // page.classList.remove(classNames.pages.active);
+      //}
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
+  initBooking: function () {
+    const thisApp = this;
+
+    /* find container for widget for booking*/
+    const bookingWidget = document.querySelector(select.containerOf.booking);
+
+    /* create new installation for class Booking */
+    thisApp.booking = new Booking(bookingWidget);
+  },
+
   initMenu: function () {
     const thisApp = this;
     //console.log('thisApp.data:', thisApp.data);
@@ -43,21 +116,23 @@ const app = {
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
-    thisApp.productList.addEventListener('add-to-cart', function(event){
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
     });
   },
 
   init: function () {
     const thisApp = this;
-    console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
-    console.log('classNames:', classNames);
-    console.log('settings:', settings);
-    console.log('templates:', templates);
+    //console.log('*** App starting ***');
+    //console.log('thisApp:', thisApp);
+    //console.log('classNames:', classNames);
+    //console.log('settings:', settings);
+    //console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initCart();
     thisApp.initData();
+    thisApp.initBooking();
   },
 };
 
